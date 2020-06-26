97a40d6c6ef568710e5c5eb66f1a67de77d61392
varun <90.varunkrishna@gmail.com>
2020-06-26 18:51:22 +0530

    static files changes made

--- dm_main/settings.py
+++ dm_main/settings.py
@@ -123,9 +123,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 # staticfiles_dirs = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'staticfiles')
-#       ] 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles')
+      ] 
 
